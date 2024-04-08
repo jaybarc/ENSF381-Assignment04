891c8b22ab1f1aa3992c9c90a5b2f6ecbef2aeb9
jaybarc <jerome.barcelona@gmail.com>
2024-04-07 18:33:43 -0600

    FINSIHED

--- my-ecommerce-app/src/App.js
+++ my-ecommerce-app/src/App.js
@@ -1,29 +1,5 @@
 import React from 'react';
 import Homepage from './components/Homepage'; 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Productpage from './components/Productpage';
-import LoginPage from './components/LoginPage';
-
-function App() {
-    return (
-        <Router>
-            <div className="App">
-                <Routes>
-                    <Route path="/" element={<Homepage />} />
-                    <Route path="/products" element={<Productpage />} />
-                    <Route path="/login" element={<LoginPage />} />
-                </Routes>
-            </div>
-        </Router>
-    );
-}
-
-export default App;
-
-
-
-/*import React from 'react';
-import Homepage from './components/Homepage'; 
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Productpage from './components/Productpage';
 import LoginPage from './components/LoginPage';
@@ -42,4 +18,4 @@ function App() {
     );
 }
 
-export default App;*/
+export default App;
--- my-ecommerce-app/src/components/Header.js
+++ my-ecommerce-app/src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './Header.module.css';
 import {Link} from 'react-router-dom';
+import { isAuthenticated } from './auth';
 
 function Header() {
   return (
@@ -16,7 +17,12 @@ function Header() {
     
       <nav className={styles.navList}>
         <Link to="/" className={styles.navItem}>Home</Link>
-        <Link to="/products" className={styles.navItem}>Products</Link>
+        {isAuthenticated() ? (
+          <Link to="/products" className={styles.navItem}>Products</Link>
+        ) : (
+          <Link to="/login" className={styles.navItem}>Products</Link>
+        )}
+
         <Link to="/login" className={styles.navItem}>Login</Link>
       </nav>
       
